03cc0344ecf27897eb5721c2919bb590b5215e06
zebestlove <zbyslav13@gmail.com>
2020-12-17 20:01:25 +0200

    just started

---
+++ me/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header-1">Who am I?</header>
+      <div className="story">
+        <p>my story</p>
+      </div>
+      <div className="city">
+        <p>my city</p>
+      </div>
+      <div className="career">
+        <p>my career</p>
+      </div>
+      <div className="instagram">
+        <p>my instagram</p>
+      </div>
+      <div className="twitter">
+        <p>my twitter</p>
+      </div>
+      <div className="email">
+        <p>my email</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
